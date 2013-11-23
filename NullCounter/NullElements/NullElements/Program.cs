e8cb0a2b4d501699a95053ae6da50ca864ab6a17
unknown <Vorona@Slav.P>
2013-11-23 16:33:12 +0400

    made in two ways

---
+++ NullCounter/NullElements/NullElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NullElements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            long length = 100000000;
+            int[] array = new int[length];
+            Random rand = new Random();
+            int t = 0;
+            stopWatch.Start();
+            while (t < 5)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    array[i] = rand.Next(0, 100);
+                }
+                t++;
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            Console.WriteLine("Time spent to fill the array in seconds is : {0} \n ", ts.TotalSeconds / 10.0);
+            stopWatch.Start();
+            long counter = array.LongCount(x => x == 0);
+            stopWatch.Stop();
+            TimeSpan ts1 = stopWatch.Elapsed;
+            Console.WriteLine("Time spent to count null elements in seconds is : {0} \n ", ts1.TotalSeconds / 10.0);
+            Console.WriteLine("Number of null elements is : {0}", counter);
+        }
+    }
+}
